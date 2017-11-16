be7bde516a21262933313d46392f069548fa140c
BRYAN <BRYAN@BRYAN-PC>
2017-11-16 19:13:36 -0400

    Changes done:
    Changed frame name, size and location

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,19 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+				
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int screenWidth = (int) screenSize.getWidth();
+		int screenHeight = (int) screenSize.getHeight();
+		
+		JFrame myFrame = new JFrame("Bryan Figueroa");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation((screenWidth/2)-100, (screenHeight/2)-100);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
